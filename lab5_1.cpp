ce9882c6d91fad995cf4b8ee7df83e0ced705b59
Thidtanai <thidtanai_k@cmu.ac.th>
2020-12-15 16:55:12 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,9 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int input = 1;
+    int Even = 0, Odd = 0; 
+    while (input != 0)
+    {
+        cout << "Enter an integer: ";
+        cin >> input;
+        if (input == 0)
+        {
+            break;
+        }
+        else if(input % 2 == 0){
+            Even += 1;
+        }
+        else{
+            Odd += 1;
+        }
+    }
+    cout << "#Even numbers = " << Even <<"\n";
+    cout << "#Odd numbers = " << Odd <<"\n";
     return 0;
 }
